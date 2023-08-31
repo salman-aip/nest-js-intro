fe86e67bd9588c5b3af739234484660cff9b13d0
Salman Farshee <salman@aveniripltd.com>
2023-08-31 17:53:58 +0600

    initialize project

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  // dependency injection in action
+  // service name : type of service (appService: AppService)
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
